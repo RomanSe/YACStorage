c66521b7c661723e9f7ae3776dff910498f02b03
Roman Semenov <rosemenov@yandex.ru>
2018-09-23 21:24:50 +0300

    Добавил два поля на форму Починил ошибку с ненахождением fxml

--- client/src/main/java/com/rs/client/Controller.java
+++ client/src/main/java/com/rs/client/Controller.java
@@ -1,4 +1,17 @@
 package com.rs.client;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
 public class Controller {
+    @FXML
+    Label loginField;
+
+    @FXML
+    PasswordField passwordField;
+
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+    }
 }
